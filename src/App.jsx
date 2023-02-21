945ecd7ce48a7de24a74cef691fadcdd0036d5a6
joshuatsai <joshuatsai1213@gmail.com>
2023-02-21 16:31:32 +0800

    利用yarn安裝bootstrap套件

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div>
     <header className="header">
     <div className="container d-flex flex-column align-items-center">
       <img className="header__avatar" src="./images/avatar.png" alt="avatar"/>
-      <h1 className="header__title">Name</h1>
+      <h1 className="header__title">ironman</h1>
       <hr className="divider--light"/>
       <p className="header__slogan">10/10</p>
     </div>
