84cfdfec68cc4b7c1c10aa026907b64e23f1587c
Joshuatsai <joshuatsai1213@gmail.com>
2023-05-01 04:07:40 +0800

    add json env

--- src/componets/Image/index.jsx
+++ src/componets/Image/index.jsx
@@ -1,120 +1,32 @@
 import style from './Image.module.css'
 import { Row, Col } from "antd";
-export default function Image() {
-    return (
-        <div>
-        <article className={style.imageLayout}>
+export default function Image({ images }) {
+  return (
+    <div>
+      <article className={style.imageLayout}>
         <div className="container">
           <h1 className="text-center">IMAGES</h1>
-          <hr className="divider--dark"/>
+          <hr className="divider--dark" />
           <Row gutter={[24, 24]}>
+          {images.map(image => (
             <Col
+              key={image.id}
               sm={{ span: 24 }}
               md={{ span: 12 }}
               lg={{ span: 8 }}
               xl={{ span: 6 }}
             >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img1.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 1</p>
+              <div className={style.image}>
+                <a href="#" className={style.imagelink}>
+                  <img src={image.image} alt={image.name} className={style.imageStyle} />
+                </a>
+                <p className={style.imageTitle}>{image.name}</p>
               </div>
             </Col>
-            <Col
-              sm={{ span: 24 }}
-              md={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }} 
-            >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img2.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 2</p>
-              </div>
-            </Col>
-            <Col
-              sm={{ span: 24 }}
-              md={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }}
-            >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img3.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 3</p>
-              </div>
-            </Col>
-            <Col
-              sm={{ span: 24 }}
-              md={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }}
-            >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img4.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 4</p>
-              </div>
-            </Col>
-            <Col
-              sm={{ span: 24 }}
-              md={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }}
-            >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img5.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 5</p>
-              </div>
-            </Col>
-            <Col
-              sm={{ span: 24 }}
-              md={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }}
-            >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img6.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 6</p>
-              </div>
-            </Col>
-            <Col
-              sm={{ span: 24 }}
-              md={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }}
-            >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img7.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 7</p>
-              </div>
-            </Col>
-            <Col
-              sm={{ span: 24 }}
-              md={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }}
-            >
-               <div className={style.image}>
-              <a href="" className={style.imagelink}>
-                <img src="images/img8.jpg" alt="" className={style.imageStyle}/>
-              </a>
-              <p className={style.imageTitle}>Place 8</p>
-              </div>
-            </Col>
+            ))}
           </Row>
         </div>
-      </article>      
-      </div>
-    );
+      </article>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import Header from "../componets/Header/index"
 import Image from "../componets/Image/index"
 import Description from "../componets/Description/index"
 import Footer from "../componets/Footer/index"
+import images from "../json/images.json"
 function Home() {
     return (
         <div ClassName="homeLayout">
         <Header />
-        <Image />              
+        <Image images={images}/>              
         <Description />
         <Footer />
         </div>
